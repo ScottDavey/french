1caf8187d6fff13d12553ac4a042981e9288ad73
Scott Davey <scottmichaeldavey@gmail.com>
2022-11-23 08:15:54 -0500

    Adding some polish

--- scripts.js
+++ scripts.js
@@ -10,17 +10,50 @@ function getRandomWords() {
         verbs: [],
         words: [],
     };
+    const randomVerbIndecies = [];
+    const randomWordIndecies = [];
+    const errorMsgEl = document.getElementById('errorMsg');
+
+    // Validate the number - make sure it's not higher than the total number of words
+    if (numberOfWords > verbs.length + 1 || numberOfWords > words.length + 1) {
+        errorMsgEl.style.display = 'inline';
+        return;
+    } else {
+        errorMsgEl.style.display = 'none';
+    }
+
+    // Loop over number of Verbs and get unique values
+    for (let i = 0; i < numberOfVerbs; i++) {
+        let randomV = rando(0, verbs.length -1);
+        
+        // Make sure it's unique. If not, get another one
+        while (randomVerbIndecies.includes(randomV)) {
+            randomV = rando(0, verbs.length -1);
+        }
+
+        randomVerbIndecies.push(randomV);
+    }
+
+    // Loop over number of words and get unique value
+    for (let j = 0; j < numberOfNonVerbs; j++) {
+        let randomW = rando(0, words.length - 1);
+
+        // Make sure it's unique. If not, get another one
+        while (randomWordIndecies.includes(randomW)) {
+            randomW = rando(0, words.length - 1);
+        }
+
+        randomWordIndecies.push(randomW);
+    }
 
     // Loop over Verbs.
-    for (let i = 0; i <= numberOfVerbs; i++) {
-        const randomIntV = rando(0, verbs.length - 1);
-        finalWordSelection.verbs.push(verbs[randomIntV]);
+    for (const vIndex of randomVerbIndecies) {
+        finalWordSelection.verbs.push(verbs[vIndex]);
     }
 
     // Loop over Non-Verb Words
-    for (let i = 0; i <= numberOfNonVerbs; i++) {
-        const randomIntW = rando(0, words.length - 1);
-        finalWordSelection.words.push(words[randomIntW]);
+    for (const wIndex of randomWordIndecies) {
+        finalWordSelection.words.push(words[wIndex]);
     }
 
     displayRandomWords(finalWordSelection);
@@ -31,9 +64,13 @@ function displayRandomWords(wordList = { verbs: [], words: [] }) {
     // Clear container
     wordEl.innerHTML = '';
 
+    // Start of word section
+    let componentV = `<div class="wordSection">`;
+
     // Loop over verbs
     for (const v of wordList.verbs) {
-        const componentV = `
+        // Create new component element
+        componentV += `
             <div class="component">
                 <div class="topBar">
                     <div class="type">VERB</div>
@@ -45,17 +82,24 @@ function displayRandomWords(wordList = { verbs: [], words: [] }) {
                 </div>
             </div>
         `;
-        // Append the new element
-        wordEl.innerHTML += componentV;
     }
+
+    // End word section
+    componentV += `</div>`;
+
+    // Append the new element
+    wordEl.innerHTML += componentV;
+
+    // Strart of next word section
+    let componentW = `<div class="wordSection">`;
 
     // Loop over words
     for (const w of wordList.words) {
-        const componentW = `
+        componentW += `
             <div class="component long">
                 <div class="topBar">
-                    <div class="type">WORD</div>
-                    <div class="group">&nbsp;</div>
+                    <div class="type">MOT</div>
+                    <div class="group">${w.gender}</div>
                 </div>
                 <div class="mid">
                     <div class="fr">${w.fr}</div>
@@ -68,9 +112,13 @@ function displayRandomWords(wordList = { verbs: [], words: [] }) {
                 }
             </div>
         `;
-        // Append the new element
-        wordEl.innerHTML += componentW;
     }
+
+    // End of word section
+    componentW += `</div>`;
+
+    // Append the new element
+    wordEl.innerHTML += componentW;
 }
 
 function main() {
